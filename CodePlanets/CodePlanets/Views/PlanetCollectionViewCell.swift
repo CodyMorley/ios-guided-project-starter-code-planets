0e05aa92ec71b196ff2cb3e7497bb14d4f035e47
Cody Morley <codymorleyinc@gmail.com>
2020-04-13 11:13:03 -0700

    finished class

--- CodePlanets/CodePlanets/Views/PlanetCollectionViewCell.swift
+++ CodePlanets/CodePlanets/Views/PlanetCollectionViewCell.swift
@@ -10,13 +10,63 @@ import UIKit
 
 class PlanetCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
+    private var imageView = UIImageView()
+    private var nameLabel = UILabel()
     
     var planet: Planet? {
         didSet {
             updateViews()
         }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpSubviews() {
+        //configure image view and use NSLayout Contraint or NSLayoutAnchor to add constraints to the imageview.
+        //1. create/configure the subview
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        
+        //2. add subview to view heirarchy
+        addSubview(imageView)
+        
+        //3. create constraints
+        // this is the y constraint
+        let imageViewYConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 4)
+        // this is the x constraint
+        let imageViewXConstraint = NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 4)
+        //this is the width constraint
+        let imageViewWidthConstraint = NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: -4)
+        //this is the height constraint
+        let imageViewHeightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: 1.0, constant: 0)
+        
+        //4. activate constraints
+        NSLayoutConstraint.activate([imageViewXConstraint, imageViewYConstraint, imageViewWidthConstraint, imageViewHeightConstraint])
+    
+        //configure the label and use the same constraint classes to make it appear centered below the imageview.
+        //1.
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textAlignment = .center
+        //2.
+        addSubview(nameLabel)
+        //3.
+        //Y
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        //X
+        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        //Width
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+        //height is set implicitly by the font size in a label
+        //4. Done already with isActive = true
+        
     }
     
     private func updateViews() {
